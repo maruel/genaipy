100241d81da5b3ec40473f4846c88a5ae80526d3
Marc-Antoine Ruel <maruel@gmail.com>
2025-08-03 11:57:50 +0100

    Initial implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,27 @@
 module github.com/maruel/genaipy
 
 go 1.24.4
+
+require (
+	github.com/maruel/genai v0.0.0-20250803104054-66b427386bd1
+	github.com/maruel/httpjson v0.5.0
+	github.com/maruel/sillybot v0.0.0-20250613193351-2e3673a7902b
+)
+
+require (
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/invopop/jsonschema v0.13.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/lmittmann/tint v1.1.0 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/maruel/roundtrippers v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	gopkg.in/dnaeon/go-vcr.v4 v4.0.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ py_test.go
@@ -0,0 +1,98 @@
+// Copyright 2024 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package genaipy
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/maruel/genai"
+	"github.com/maruel/genai/providers/openaicompatible"
+	"github.com/maruel/httpjson"
+)
+
+func TestNewServer(t *testing.T) {
+	if os.Getenv("CI") == "true" {
+		// Sadly github is too slow.
+		t.Skip("skipping test in CI environment")
+	}
+	t.Parallel()
+	ctx := context.Background()
+	// To run from scratch every time, but it's a bit slow:
+	// cache := t.TempDir()
+	cache, err := filepath.Abs("../cache/py")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = os.MkdirAll(cache, 0o755); err != nil {
+		t.Fatal(err)
+	}
+
+	port := strconv.Itoa(findFreePort())
+	// This is a very slow test.
+	srv, err := NewServer(ctx, "llm.py", cache, filepath.Join(cache, "py_llm.py"), []string{"--port", port})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		if err2 := srv.Close(); err2 != nil {
+			t.Error(err2)
+		}
+	})
+
+	client, err := openaicompatible.New("http://localhost:"+port+"/v1/chat/completions", nil, "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	start := time.Now()
+	for {
+		resp, err := client.GenSync(ctx, genai.Messages{
+			genai.NewTextMessage(genai.User, "Say hello. Reply with only one word."),
+		}, nil)
+		var v *url.Error
+		var h *httpjson.Error
+		if errors.As(err, &v) || errors.As(err, &h) {
+			// It can be slow when run from scratch, especially on github.
+			if time.Since(start) > 5*time.Minute {
+				break
+			}
+			select {
+			case <-srv.Done():
+				t.Fatal("server died")
+			case <-time.After(10 * time.Millisecond):
+			}
+			continue
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+		txt := strings.TrimSpace(resp.Contents[0].Text)
+		txt = strings.TrimRight(txt, ".!")
+		txt = strings.ToLower(txt)
+		if txt != "hello" {
+			t.Errorf("got %q, want %q", txt, "hello")
+		}
+		return
+	}
+	t.Fatal("too many retries")
+}
+
+func findFreePort() int {
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port
+}
